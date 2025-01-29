5f0454853c66555a7913e5b035b52540adde1e1d
Volodymyr <premisterio007@gmail.com>
2025-01-29 15:31:30 +0200

    Finished palindrome checker
    Changes:
    - Finished main functionality
    - Fixed minor bugs

--- javaScript/palindrome-checker/script.js
+++ javaScript/palindrome-checker/script.js
@@ -1,19 +1,9 @@
-// the code must to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
-// remove hidden class to display the result
-
-// get the elements
-const palindromeForm = document.getElementById("palindrome-form");
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 function checkPalindrome() {
-  const inputValue = textInput.value;
-  if (inputValue === "") {
-    alert("Please enter a value!");
-    return;
-  }
-  const cleanedInput = inputValue.replace(/[^a-zA-Z0-9]/g, "").toLowerCase(); 
+  const cleanedInput = textInput.value.replace(/[^a-zA-Z0-9]/g, "").toLowerCase(); 
   const isPalindrome = cleanedInput === cleanedInput.split("").reverse().join("");
   return isPalindrome;
 }
@@ -21,6 +11,9 @@ function checkPalindrome() {
 function returnResult() {
     const isPalindrome = checkPalindrome();
     if (textInput.value === "") {
+        setTimeout(() => {
+            alert("Please input a value");
+        }, 100);
         return;
     } else if (isPalindrome) {
         result.classList.remove("hidden");
